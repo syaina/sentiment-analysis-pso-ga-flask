868b28e24ac9c65d92d4fefa09ffcbc0550f4094
syaina <syainanurf@gmail.com>
2021-06-28 01:54:28 +0700

    finalization

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ sf_ga = ga_model_load[1]
 
 fit_sc = pickle_load(app.config['SAVED_FOLDER'], '/fit_sc')
 
+X = dataset_load[0][:100]
+y = dataset_load[1][:100]
 # =========================================================================================================
 def build_word_vector(tokens, size, w2v_model):
     vec = np.zeros(size).reshape((1, size))
@@ -105,25 +107,9 @@ def get_pengujian(filename):
 
     return pengujian
 
-def get_X_y():
-    preprocessing_result = preprocessing(raw_df)
-    df = preprocessing_result[-1]
-    X = np.concatenate([build_word_vector(z, 200, w2v_model) for z in map(lambda x: x, df['sastrawi_stopword_removal'])])
-    sc = StandardScaler()
-    fit_sc = sc.fit(X)
-    X = sc.transform(X)
-
-    labelEncoder = LabelEncoder()
-    y = labelEncoder.fit_transform(df['sentiment'])
-
-    df_train = pd.DataFrame(X)
-    df_train['target'] = pd.DataFrame(y)
-
-    return X, y, df_train
-
 def get_X_y_smote():
-    X_smote = dataset_load[0][:50]
-    y_smote = dataset_load[1][:50]
+    X_smote = dataset_load[0][:100]
+    y_smote = dataset_load[1][:100]
 
     df_train_smote = pd.DataFrame(X_smote)
     df_train_smote['target'] = pd.DataFrame(y_smote)
@@ -186,7 +172,156 @@ def ga_predict(sentence):
     predict = ga_model.predict(vect_sentence_norm_fs.reshape(-1,124))
     return predict[0]
     
+# =======================================================================================================
+# SIMULASI FITUR SELEKS
 
+kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+kernel = 'rbf'
+C = 4
+GAMMA = 0.01
+model = SVC(kernel=kernel, C=C, gamma=GAMMA)
+
+def kfold_train(X, y):
+  cv_acc = []
+  
+  for train, test in kfold.split(X, y): 
+    X_train, X_test = X[train], X[test]
+    y_train, y_test = y[train], y[test]
+
+    model_fit = model.fit(X_train, y_train)
+    y_pred = model_fit.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    cv_acc.append(acc)
+    
+  return np.array(cv_acc).mean()
+
+def kfold_training(X, y):
+    cv_cr = []
+
+    for train, test in kfold.split(X, y): 
+        X_train, X_test = X[train], X[test]
+        y_train, y_test = y[train], y[test]
+
+        model_fit = model.fit(X_train, y_train)
+        y_pred = model_fit.predict(X_test)
+        cr = classification_report(y_test, y_pred, output_dict=True)
+        cv_cr.append(cr)
+
+    return cv_cr
+
+def get_score(cr):
+    recall_0 = []
+    precision_0 = []
+    f1_0 = []
+
+    recall_1 = []
+    precision_1 = []
+    f1_1 = []
+
+    recall_2 = []
+    precision_2 = []
+    f1_2 = []
+
+    recall = []
+    precision = []
+    f1 = []
+    accuracy = []
+
+    for i in range(len(cr)):
+        recall_0.append(cr[i]['0']['recall'])
+        precision_0.append(cr[i]['0']['precision'])
+        f1_0.append(cr[i]['0']['f1-score'])
+
+        recall_1.append(cr[i]['1']['recall'])
+        precision_1.append(cr[i]['1']['precision'])
+        f1_1.append(cr[i]['1']['f1-score'])
+
+        recall_2.append(cr[i]['2']['recall'])
+        precision_2.append(cr[i]['2']['precision'])
+        f1_2.append(cr[i]['2']['f1-score'])
+
+        recall.append(recall_0)
+        recall.append(recall_1)
+        recall.append(recall_2)
+
+        precision.append(precision_0)
+        precision.append(precision_1)
+        precision.append(precision_2)
+
+        f1.append(f1_0)
+        f1.append(f1_1)
+        f1.append(f1_2)
+    
+        accuracy.append(cr[i]['accuracy'])
+
+    accuracy = round((np.array(accuracy).mean() * 100) ,2)
+    recall = round((np.array(recall).mean() * 100), 2)
+    precision = round((np.array(precision).mean() * 100), 2)
+    f1 = round((np.array(f1).mean() * 100), 2)
+
+    return accuracy, recall, precision, f1
+
+def f_per_particle(m, alpha):
+    total_features = X.shape[1]
+    if np.count_nonzero(m) == 0: 
+        X_subset = X
+    else:
+        X_subset = X[:,m==1]
+
+    P = kfold_train(X_subset, y)
+    fitness = (alpha * (1.0 - P) + (1.0 - alpha) * (1 - (X_subset.shape[1] / total_features)))
+
+    return fitness
+
+def f(x, alpha=0.9):
+    n_particles = x.shape[0]
+    j = [f_per_particle(x[i], alpha) for i in range(n_particles)]
+    return np.array(j)
+
+def running_pso(n_particles):
+    options = { 'c1': 2, 
+                'c2': 2, 
+                'w': 0.9, 
+                'k': 3, 
+                'p': 1}
+
+    dimensions = X.shape[1]
+    optimizer = ps.discrete.BinaryPSO(n_particles=n_particles, dimensions=dimensions, options=options)
+    cost, pos = optimizer.optimize(f, iters=10, verbose=2)
+    return pos, cost
+
+class CustomFitnessFunctionClass:
+    def __init__(self,n_total_features,n_splits = 5, alpha=0.01, *args,**kwargs):
+        
+        self.n_splits = n_splits
+        self.alpha = alpha
+        self.n_total_features = n_total_features
+
+    def calculate_fitness(self,model,x,y):
+        alpha = self.alpha
+        total_features = self.n_total_features
+
+        P = kfold_train(x,y)
+        fit = (alpha*(1.0 - P) + (1.0 - alpha)*(1.0 - (x.shape[1])/total_features))
+        fitness = 1 - fit
+        return fitness
+
+def running_ga(n_pop):
+    alpha = 0.9
+    ff = CustomFitnessFunctionClass(n_total_features=X.shape[1], n_splits=10, alpha=alpha)
+    fsga = FeatureSelectionGA(model, X, y, verbose=1, ff_obj=ff)
+    pop = fsga.generate(n_pop=n_pop, ngen=10, mutxpb=0.2)
+
+    if (len(pop) > 1):
+      pos = np.array(pop[0])
+    else : 
+      pos = np.array(pop)
+    X_subset = X[:, pos==1]
+    P = kfold_train(X_subset,y)
+    fit = (alpha*(1.0 - P) + (1.0 - alpha)*(1.0 - (X_subset.shape[1])/X.shape[1]))
+    fitness = 1 - fit
+
+    return pos, fitness
 # =======================================================================================================
 
 @app.route("/")
@@ -227,15 +362,85 @@ def show_pengolahan_data():
         epoch_w2v=epoch_w2v,
         data_train=data_train)
     
-@app.route("/fitur-seleksi")
+@app.route("/fitur-seleksi", methods=['GET', 'POST'])
 def show_fitur_seleksi():
     particle_pso = get_pengujian('pengujian-particle_pso.csv')
     populasi_ga = get_pengujian('pengujian-populasi_ga.csv')
 
-    return render_template('fitur-seleksi.html',
-        particle_pso=particle_pso,
-        populasi_ga=populasi_ga
-        )
+    if request.method == 'POST':
+        fs = request.form['fs']
+
+        if(fs == 'fs-pso'):
+            pso_result = True
+            ga_result = False
+            n_pop = None
+
+            particles = request.form['particle']
+            n_particles = np.array(particles).astype(np.int)
+            pso_result = running_pso(n_particles)
+            pos = pso_result[0]
+            cost = round(pso_result[1],3)
+
+            train = kfold_training(X[:, pos==1],y)
+            score = get_score(train)
+            accuracy = score[0]
+            recall = score[1]
+            precision = score[2]
+            f1 = score[3]
+
+            return render_template('fitur-seleksi.html',
+                particle_pso=particle_pso,
+                populasi_ga=populasi_ga,
+                pso_result=pso_result,
+                ga_result=ga_result,
+                pos=pos,
+                cost=cost,
+                n_particles=n_particles,
+                n_pop=n_pop,
+                accuracy=accuracy,
+                recall=recall,
+                precision=precision,
+                f1=f1
+                )
+
+        elif(fs == 'fs-ga'):
+            ga_result = True
+            pso_result = False
+            n_particles = None
+
+            populasi = request.form['populasi']
+            n_pop = np.array(populasi).astype(np.int)
+            ga_result = running_ga(n_pop)
+            pos = ga_result[0]
+            cost = round(ga_result[1],3)
+
+            train = kfold_training(X[:, pos==1],y)
+            score = get_score(train)
+            accuracy = score[0]
+            recall = score[1]
+            precision = score[2]
+            f1 = score[3]
+
+            return render_template('fitur-seleksi-ga.html',
+                particle_pso=particle_pso,
+                populasi_ga=populasi_ga,
+                pso_result=pso_result,
+                ga_result=ga_result,
+                pos=pos,
+                cost=cost,
+                n_particles=n_particles,
+                n_pop=n_pop,
+                accuracy=accuracy,
+                recall=recall,
+                precision=precision,
+                f1=f1
+                )
+
+    else:
+        return render_template('fitur-seleksi.html',
+            particle_pso=particle_pso,
+            populasi_ga=populasi_ga
+            )
 
 @app.route("/training-evaluasi")
 def show_training_evaluasi():
@@ -250,7 +455,24 @@ def show_training_evaluasi():
         fold_pso=fold_pso,
         fold_ga=fold_ga)
 
-@app.route("/prediksi-sentimen")
+@app.route("/prediksi-sentimen", methods=['GET', 'POST'])
 def show_prediksi_sentimen():
-    return render_template('prediksi-sentimen.html')
+    if request.method == 'POST':
+        sentiment = request.form['sentiment']
+        model = request.form['model']
+
+        if(model == 'svm'):
+            predict = svm_predict(sentiment)
+        elif(model == 'pso'):
+            predict = pso_predict(sentiment)
+        elif(model == 'ga'):
+            predict = ga_predict(sentiment)
+
+        return render_template('prediksi-sentimen.html', 
+            sentiment=sentiment,
+            model=model,
+            predict=predict)
+    
+    else:
+        return render_template('prediksi-sentimen.html')
 
